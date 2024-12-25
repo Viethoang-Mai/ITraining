8b8ce1e71789c45ef8c0b7f653c86060a24cd445
ViethoangMai-dev <viethoangmai21199@.com>
2024-12-25 17:17:38 +0700

    Training 25/12/2024 :Học Javascript 90%

--- Javascript/dom/js/ex15.js
+++ Javascript/dom/js/ex15.js
@@ -1,120 +1,167 @@
-var root = document.querySelector("#root");
-// h1 = document.createElement("h1");
-// h1.innerText = "Hello anh em: ";
+/*
+Web Component: Kỹ thuật tách các thành phần thành các thẻ html riêng biệt để thuận tiện trong việc tái sử dụng
+*/
+// console.dir(HTMLElement);
+// var h1 = document.createElement("h1");
+// console.dir(h1);
+//HTMLHeadingElement --> HTMLElement ->  Element --> Node --> EventTarget
+// h1.addEventListener('');
+// HTMLDivElement.prototype.css = function (input = {}) {
+//   Object.assign(this.style, input);
+// };
 
-// var textNode = document.createTextNode(0);
-// h1.append(textNode);
-
-// root.append(h1);
-
-// var button = document.createElement("button");
-// button.innerText = "Click me";
-// root.append(button);
-
-// button.addEventListener("click", function () {
-//   textNode.data++;
+// document.querySelector(".content").css({
+//   color: "red",
 // });
 
-// //Comment Node
-// var comment = document.createComment("Xin chào F8");
-// root.append(comment);
+//Các bước để tạo ra 1 component
 
-// //Clone Node
-// var h2 = document.createElement("h2");
-// h2.innerText = "Học Javascript";
-// root.append(h2);
-// root.append(h2.cloneNode(true));
-// root.append(h2.cloneNode(true));
-// root.append(h2.cloneNode(true));
-// root.append(h2.cloneNode(true));
-// root.append(h2.cloneNode(true));
+//B1. Tạo ra 1 constructor / class kế thừa từ HTMLElement
 
-var users = [
-  {
-    id: 1,
-    name: "User 1",
-  },
-  {
-    id: 2,
-    name: "User 2",
-  },
-  {
-    id: 3,
-    name: "User 3",
-  },
-];
+//B2. Đưa constructor vào phương thức customElement.define()
 
-var render = function () {
-  var html = users
-    .map(function (user) {
-      return `<h3>${
-        user.name
-        // .replaceAll("<", "&lt;")
-        // .replaceAll(">", "&gt;")
-      }</h3>`;
-    })
-    .join("");
-  var form = `<input type="text" class="name" placeholder="Nhập tên..." /> <button>Thêm</button>`;
-  root.innerHTML = form + html;
-};
-render();
+// class HelloWorld extends HTMLElement {
+//   static observedAttributes = ["data-id"];
+//   connectedCallback() {
+//     console.log("Component được đưa vào DOM");
+//     var _this = this;
+//     this.innerHTML = "<h1>Học lập trình không khó</h1>";
+//     this.querySelector("h1").addEventListener("click", function () {
+//       _this.setAttribute("data-id", Math.random());
+//     });
+//   }
+//   disconnectedCallback() {
+//     console.log("Component được loại bỏ khỏi DOM");
+//   }
+//   attributeChangedCallback(name, oldValue, newValue) {
+//     //Khi thuộc tính thay đổi
+//     console.log("Thuộc tính thay đổi");
+//     console.log(name, oldValue, newValue);
+//   }
+// }
+// customElements.define("hello-world", HelloWorld);
 
-var btn = root.querySelector("button");
-var input = root.querySelector("input");
-btn.addEventListener("click", function () {
-  var name = input.value;
-  users.push({
-    id: users.length,
-    name: name,
-  });
-  render();
+// var helloWorldEl = document.createElement("hello-world");
+// var btn = document.querySelector("button");
+// var isShow = false;
+// btn.addEventListener("click", function () {
+//   if (!isShow) {
+//     document.body.append(helloWorldEl);
+//     isShow = true;
+//   } else {
+//     helloWorldEl.remove();
+//     isShow = false;
+//   }
+// });
+
+customElements.define(
+  "todo-app",
+  class extends HTMLElement {
+    connectedCallback() {
+      var shadow = this.attachShadow({
+        mode: "open",
+      });
+      this.render(shadow);
+      this.initialStyle(shadow);
+      this.addEvent(shadow);
+      //   var loadEvent = new Event("todo-loaded");
+      //   var ul = shadow.querySelector("ul");
+      //   loadEvent.todoCount = ul.children.length;
+      //   this.dispatchEvent(loadEvent);
+    }
+    render(shadow) {
+      var html = `
+      <div class="todo-app">
+        <ul>
+            <li>Công việc 1</li>
+            <li>Công việc 2</li>
+            <li>Công việc 3</li>
+        </ul>
+        <input type="text" placeholder="Tên..."/>
+        <button>Thêm</button>
+      </div>
+      `;
+      shadow.innerHTML = this.innerHTML + html;
+    }
+    initialStyle(shadow) {
+      var style = document.createElement("style");
+      style.textContent = `
+      .todo-app {
+        background: ${this.dataset.bg};
+        padding: 15px;
+      }
+      `;
+      shadow.append(style);
+    }
+    addEvent(shadow) {
+      var btn = shadow.querySelector("button");
+      var _this = this;
+      btn.addEventListener("click", function () {
+        _this.handleAdd(shadow);
+      });
+    }
+    handleAdd(shadow) {
+      var ul = shadow.querySelector("ul");
+      var input = shadow.querySelector("input");
+      var value = input.value;
+      if (!value) {
+        alert("Vui lòng nhập tên công việc");
+        return;
+      }
+      var li = document.createElement("li");
+      li.innerText = value;
+      ul.append(li);
+      input.value = "";
+
+      //Gửi event vào component todo-app để bất kỳ nơi nào cũng lắng nghe được
+      //Tạo event tên là todo-change --> Gán vào thẻ todo-app
+      var todoChangeEvent = new Event("todo-change");
+      todoChangeEvent.todoCount = ul.children.length;
+      this.dispatchEvent(todoChangeEvent);
+    }
+  }
+);
+
+customElements.define(
+  "todo-count",
+  class extends HTMLElement {
+    connectedCallback() {
+      var shadow = this.attachShadow({ mode: "open" });
+      var todoAppEl = document.querySelector("todo-app");
+
+      shadow.innerHTML = `<h3>Count: <span>${
+        todoAppEl.shadowRoot.querySelector("ul").children.length
+      }</span></h3>`;
+
+      todoAppEl.addEventListener("todo-change", function (e) {
+        shadow.querySelector("span").innerText = e.todoCount;
+      });
+    }
+  }
+);
+
+var todoAppEl = document.querySelector("todo-app");
+todoAppEl.addEventListener("todo-change", function (e) {
+  console.log(e.todoCount);
 });
 
-// <img onerror="window.location.href='https://fullstack.edu.vn'" src="abc" />
-//
+//Shadow DOM:
 
-//Class
-class User {
-  //Thuộc tính
-  constructor() {
-    this.name = "Hoàng An";
-    this.email = "hoangan.web@gmail.com";
-  }
-  //Phương thức
-  getName() {
-    return this.name;
-  }
-  getEmail() {
-    return this.email;
-  }
-}
+//Document ==> Host (Element) ==> Root ==> Content
 
-class Auth extends User {
-  getProfile() {
-    const user = {
-      name: this.getName(),
-      email: this.getEmail(),
-    };
-    return user;
-  }
-}
-
-//Tạo object
-var auth = new Auth();
-console.log(auth.getProfile());
-console.log(auth.name);
-
-/*
-Web Component: 
-- HTMLElement
-- Custom Element
-- callback lifecycle
-- Shadow DOM
-*/
-
-class HelloWorld extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `Xin chào anh em F8`;
-  }
-}
-customElements.define("hello-world", HelloWorld);
+// var content = document.querySelector(".content");
+// var shadow = content.attachShadow({
+//   mode: "open",
+// });
+// shadow.innerHTML = content.innerHTML;
+// content.innerText = "";
+// var h1 = document.createElement("h1");
+// h1.innerText = "Học lập trình không khó";
+// shadow.prepend(h1);
+// var style = document.createElement("style");
+// style.textContent = `
+// h1 {
+//     color: red;
+// }
+// `;
+// shadow.append(style);
